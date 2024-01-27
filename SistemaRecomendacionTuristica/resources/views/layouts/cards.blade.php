d4d2015f2f5b2a84ac5772f5083970fcfa8e0ab3
Denny Gutrie <gutriedenny@gmail.com>
2024-01-27 16:25:50 -0600

    Barra de busqueda y cards

--- SistemaRecomendacionTuristica/resources/views/layouts/cards.blade.php
+++ SistemaRecomendacionTuristica/resources/views/layouts/cards.blade.php
@@ -3,14 +3,21 @@
 <html lang="es">
     <head>
         <link rel="stylesheet" href="{{ asset('assets/cards.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/searchbar.css') }}" />
     </head>
 
     <body>
         <div class="wrapper">
-            <div class="cards">@yield('lugares')</div>
+            <div class="search">
+            <button id="search-toggle"><i class="fa-solid fa-magnifying-glass"></i></button>
+                @yield('search-bar')
+            </div>
+            <div class="cards">
+                @yield('lugares')
+            </div>
         </div>
     </body>
-    
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
     <script>
         const likeButtons = document.querySelectorAll(".card__btn");
         likeButtons.forEach((likeButton) => {
@@ -18,19 +25,18 @@
                 likeButton.classList.toggle("card__btn--like");
             });
         });
+    
+    document.getElementById('search-toggle').addEventListener('click', function() {
+    var button = document.getElementById('search-toggle');
+    var searchBar = document.getElementById('searchBar');
+    if (searchBar.style.display === 'none' || searchBar.style.display === '') {
+        searchBar.style.display = 'block';
+        button.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    } else {
+        searchBar.style.display = 'none';
+        button.innerHTML = '<i class="fa-solid fa-magnifying-glass">'
+    }
+});
     </script>
-    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-        aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header text-center">
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body d-flex align-items-center justify-content-center text-center">
-                    @yield('modal-tbody')
-                </div>
-            </div>
-        </div>
-    </div>
 </html>
 @endsection
--- SistemaRecomendacionTuristica/resources/views/welcome.blade.php
+++ SistemaRecomendacionTuristica/resources/views/welcome.blade.php
@@ -1,133 +1,30 @@
 @extends('layouts.cards')
-
-@section('lugares')
-
-@foreach ($lugares as $lugar)
-        <article class="card">
-            <div class="card__preview">
-                <img src="{{ asset($lugar->imagen) }}" alt=" ">
-                <div class="card__price">
-                    {{ $lugar->categoria->nombre }}
-                </div>
-            </div>
-            <div class="card__content">
-
-
-                <h2 class="card__title">{{ $lugar->nombre }}</h2>
-                <p class="card__address">
-                    Costa Rica - {{ $lugar->ubicacion }}
-                </p>
-                <p class="card__description">
-                    {{ $lugar->descripcion }}
-                </p>
-
-                <div hidden></div>
-
-                <div class="card__bottom">
-                    <div class="card__properties">
-
-                    </div>
-
-
-                    <button class="card__btn" data-bs-toggle="modal" data-bs-target="#staticBackdrop"
-                            data-nombre="{{$lugar->nombre}}"
-                            data-descripcion="{{ $lugar->descripcion }}" data-ubicacion="{{ $lugar->ubicacion }}"
-                            >
-                            <a href="#m3-o">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                            stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
-                        </svg>
-                        </a>
-                    </button>
-                </div>
-            </div>
-        </article>
-    @endforeach
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    <script>
-            $(document).ready(function() {
-                // Capturar el clic en el botón "Editar"
-                $('.card__btn').click(function(e) {
-                    e.preventDefault();
-
-                    var name = $(this).data('nombre')
-                    var descrip = $(this).data('descripcion')
-                    var ubic = $(this).data('ubicacion')
-
-                    $('.card__title').text(name);
-                    $('.card__address').text(ubic);
-                    $('.card__description').text(descrip);
-                    // Mostrar el modal de edición
-                    $('.modal').modal('show');
-                });
-            });
-    </script>
-    @section('modal-tbody')
-    <div class="card__content">
-        <h2 class="card__title"></h2>
-        <p class="card__address">
-        </p>
-        <p class="card__description">
-        </p>
-    </div>
-    @endsection
+@section('search-bar')
+<input type="text" name="text" placeholder="Categoria" class="input" id="searchBar">
 @endsection
-<!--
-
-<div class="box">
-  <a href="#m3-o" class="link-1" id="m3-c">Modal 3</a>
-  <p class="box__info">With Background Full Opacity</p>
-
-  <div class="modal-container" id="m3-o" style="--m-background: var(--global-background);">
-    <div class="modal" style="--m-shadow: 0 0 10rem 0">
-      <h1 class="modal__title">Modal 3 Title</h1>
-      <p class="modal__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ex dicta maiores libero minus obcaecati iste optio, eius labore repellendus.</p>
-      <button class="modal__btn">Button &rarr;</button>
-      <button class="modal__btn">Button &rarr;</button>
-      <a href="#m3-c" class="link-2"></a>
-    </div>
-  </div>
+@section('lugares')
+@foreach($lugares as $lugar)
+<div class="container py-4">
+		<article class="postcard light blue">
+			<div class="postcard__img_link">
+				<img class="postcard__img" src="{{asset('images/preview1.jpg')}}" alt="Image Title" />
+			</div>
+			<div class="postcard__text">
+				<h1 class="postcard__title"><a href="#">{{$lugar->nombre}}</a></h1>
+				<div class="postcard__subtitle small">
+                    {{$lugar->ubicacion}} / {{$lugar->categoria->nombre}}
+				</div>
+				<div class="postcard__bar"></div>
+				<div class="postcard__preview-txt">{{$lugar->descripcion}}</div>
+				<ul class="postcard__tagbox">
+					<li class="tag__item"><i class="fas fa-tag mr-2"></i>Podcast</li>
+					<li class="tag__item"><i class="fas fa-clock mr-2"></i>55 mins.</li>
+					<li class="tag__item">
+						<a href="#"><i class="fas fa-play mr-2"></i>Play Episode</a>
+					</li>
+				</ul>
+			</div>
+		</article>
 </div>
-@foreach ($lugares as $lugar)
-                <tr>
-                <td>{{ $lugar->id }} </td>
-                <td>{{ $lugar->nombre }} </td>
-                <td>{{ $lugar->descripcion }} </td>
-                <td>{{ $lugar->ubicacion }} </td>
-                <td >
-                    <img style="border-radius: 10px; overflow: hidden;" src="{{ asset($lugar->imagen) }}" alt="Imagen del lugar">
-                </td>
-                <td>{{ $lugar->estado->nombre }}</td>
-                <td>{{ $lugar->categoria->nombre }}</td>
-
-                <td>
-                    <div class="d-flex ">
-                        <a href="" class="editar-btn" data-bs-toggle="modal" data-bs-target="#staticBackdrop"
-                            data-id="{{ $lugar->id }}"
-                            data-nombre="{{ $lugar->nombre }}"
-                            data-descripcion="{{ $lugar->descripcion }}"
-                            data-ubicacion="{{ $lugar->ubicacion }}"
-                            data-idestado="{{ $lugar->idEstado }}"
-                            data-idcategoria="{{ $lugar->idCategoria }}"
-
-                            >
-                            <i class="fa-solid fa-pen-to-square"></i>
-                        </a>
-
-                        <form method="POST" action="{{ route('eliminarLugar', $lugar->id) }}" class="ms-2">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" style="border: none;
-                            outline: none;">
-                                <i class="fa-solid fa-trash" style="color: #f50000;"></i>
-                            </button>
-                        </form>
-                    </div>
-                </td>
-                </tr>
-        @endforeach
-
---->
+    @endforeach
+@endsection
